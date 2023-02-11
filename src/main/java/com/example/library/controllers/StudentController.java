90e16a30adac894d1ca63fb9809132c759d440cf
Rajeshwar Ojha <rajeshwar.ee17@iitp.ac.in>
2023-02-12 00:01:56 +0530

    Initial commit

---
+++ src/main/java/com/example/library/controllers/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.library.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    // GET, POST, PUT, DELETE
+
+    @GetMapping("/hello")
+    public String helloPrinter() {
+        // camel case -> camelCase -> naming in Java
+        // snake case -> snake_case -> json for interacting with the outside world
+        return "Hello Ayush this is your first API";
+    }
+
+    @GetMapping("/user")
+    public String helloUser(@RequestParam("user_name") String userName, @RequestParam("input_1") Integer input1) {
+
+        String response = "Hi " + userName + " you are welcome to our second API";
+        return response;
+
+    }
+
+
+}
