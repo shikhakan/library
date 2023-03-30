e48341935bd83d0033f996a73f28ac3b2761dec3
Rajeshwar Ojha <rajeshwar.ee17@iitp.ac.in>
2023-03-30 23:55:13 +0530

    Shikha's first lesson

--- src/main/java/com/example/library/controllers/StudentController.java
+++ src/main/java/com/example/library/controllers/StudentController.java
@@ -12,20 +12,36 @@ public class StudentController {
 
     // GET, POST, PUT, DELETE
 
+    // website -> www.library.com
+    // controller -> www.library.com/student
+    // API -> www.library.com/student/hello
+
     @GetMapping("/hello")
     public String helloPrinter() {
         // camel case -> camelCase -> naming in Java
         // snake case -> snake_case -> json for interacting with the outside world
-        return "Hello Ayush this is your first API";
+        return "Hello Shikha this is your first API";
     }
 
-    @GetMapping("/user")
-    public String helloUser(@RequestParam("user_name") String userName, @RequestParam("input_1") Integer input1) {
+    // TODO Write API like the above for printing Hi
+
+    @GetMapping("/hello-user")
+    public String helloUser(@RequestParam("user_name") String userName) {
 
         String response = "Hi " + userName + " you are welcome to our second API";
         return response;
 
     }
 
+    // TODO Write API to take the name of 3 friends and print Hi <friend name>, <friend name>, <friend name>
+
+    @GetMapping("/69-adder")
+    public int calculator(@RequestParam Integer num1) {
+        return num1 + 69;
+    }
+
+    // TODO Input 2 numbers and return their sum
+
+    // TODO Input 4 numbers and return their sum
 
 }
