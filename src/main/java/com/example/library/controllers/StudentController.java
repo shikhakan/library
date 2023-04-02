304f90f28e9cb3c09de697a71484af8bdbb0ea69
shikhakan <kanojiyashikha121198@gmail.com>
2023-04-02 17:51:09 +0530

    First task

--- src/main/java/com/example/library/controllers/StudentController.java
+++ src/main/java/com/example/library/controllers/StudentController.java
@@ -24,6 +24,11 @@ public class StudentController {
     }
 
     // TODO Write API like the above for printing Hi
+    @GetMapping("/print-hi")
+    public String printingHi() {
+        return "hi Guys, How are you?";
+    }
+
 
     @GetMapping("/hello-user")
     public String helloUser(@RequestParam("user_name") String userName) {
@@ -34,6 +39,13 @@ public class StudentController {
     }
 
     // TODO Write API to take the name of 3 friends and print Hi <friend name>, <friend name>, <friend name>
+    @GetMapping("/friends-forever")
+    public String friendsForever(@RequestParam("friend_name1") String friendName1, @RequestParam("friend_name2") String friendName2, @RequestParam("friend_name3") String friendName3) {
+
+        String introduction = "Hi" + friendName1 + friendName2 + friendName3 + "Hello Fraands";
+        return introduction;
+    }
+
 
     @GetMapping("/69-adder")
     public int calculator(@RequestParam Integer num1) {
@@ -42,6 +54,19 @@ public class StudentController {
 
     // TODO Input 2 numbers and return their sum
 
-    // TODO Input 4 numbers and return their sum
+    @GetMapping("/add-2-numbers")
+    public int add(@RequestParam Integer num1, @RequestParam Integer num2) {
+        int sum = num1 + num2;
+        return sum;
 
+    }
+
+    // TODO Input 4 numbers and return their sum
+    @GetMapping("/add-4-numbers")
+    public int addition(@RequestParam Integer num1, @RequestParam Integer
+            num2, @RequestParam Integer num3, @RequestParam Integer num4) {
+        int sum = num1 + num2 + num3 + num4;
+        return sum;
+
+    }
 }
